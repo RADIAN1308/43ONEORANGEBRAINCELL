992fa6522d11487eb7006ba6f6786a313eac0a79
aryan <aryanarun133@gmail.com>
2025-04-12 07:47:37 +0530

    commit

--- src/flet_app/app.py
+++ src/flet_app/app.py
@@ -6,20 +6,13 @@ global url
 
 url = ""
 
-# url = "https://9314-203-145-52-219.ngrok-free.app/predict"
-
 def call_api(input_string):
     headers = {"Content-Type": "application/json"}
     payload = {"input_data": input_string}
 
     try:
-        # Convert payload to JSON string and encode to bytes
         data = json.dumps(payload).encode('utf-8')
-
-        # Create request object
         req = urllib.request.Request(url, data=data, headers=headers, method="POST")
-
-        # Send request and get response
         with urllib.request.urlopen(req) as response:
             response_data = response.read().decode('utf-8')
             return json.loads(response_data)
@@ -28,59 +21,22 @@ def call_api(input_string):
     except Exception as e:
         return {"error": str(e)}
 
-
 def main(page: Page):
     page.theme_mode = ThemeMode.DARK
-    page.title = ""
+    page.title = "SLM FindMyFunds"
     page.horizontal_alignment = CrossAxisAlignment.CENTER
 
     page.update()
 
     global url
-
     bar_cont = Container()
-
 
     def update_url(e):
         global url
         url = e.control.value.strip()
         print(f"URL: {url}")
 
-
     def result_handler(e):
-        page.controls.clear()
-        page.add(
-            Row(
-                alignment=MainAxisAlignment.SPACE_BETWEEN,
-                controls=[
-                    Container(height=40),
-                    Container(
-                        content=IconButton(Icons.MENU, on_click=lambda e: page.open(drawer))
-                    ),
-                    Row(
-                        controls=[
-                            IconButton(Icons.LIGHT_MODE_OUTLINED, on_click=toggle_theme),
-                            IconButton(Icons.CONTACT_SUPPORT, on_click=lambda _: open_web)
-                        ]
-                    )
-                ]
-            ),
-            Row(
-                alignment=MainAxisAlignment.CENTER,
-                controls=[
-                    c_val,
-                    ElevatedButton(text="Submit", on_click=result_handler),
-                ]
-            ),
-            Row(
-                alignment=MainAxisAlignment.CENTER,
-                controls=[
-                    bar_cont,
-                ]
-            )
-        )
-        page.update()
-
         bar_cont.content = None
         input_string = c_val.value.strip()
         if not url:
@@ -96,8 +52,7 @@ def main(page: Page):
             page.update()
             return
 
-        lv = ListView(expand=1, spacing=10, padding=20, auto_scroll=False)
-
+        lv.controls.clear()
         for company in results:
             c_comp = Container(
                 content=Column(
@@ -115,18 +70,7 @@ def main(page: Page):
             )
             lv.controls.append(c_comp)
 
-        page.add(lv)
-
-
-    def handle_change(e):
-        if e.control.selected_index == 0:
-            page.controls.clear()
-            page.controls.append(home_screen)
-        elif e.control.selected_index == 1:
-            page.controls.clear()
-            page.controls.append(search_screen)
         page.update()
-
 
     def toggle_theme(e):
         if page.theme_mode == ThemeMode.LIGHT:
@@ -134,127 +78,84 @@ def main(page: Page):
         else:
             page.theme_mode = ThemeMode.LIGHT
         page.update()
-# oogabooga
-    def switch_to_search():
-        page.controls.clear()
-        page.controls.append(search_screen)
-        page.update()
 
     drawer = NavigationDrawer(
-        on_change=handle_change,
         controls=[
-            Container(height=12),
             NavigationDrawerDestination(
                 label="Home",
                 icon=Icons.HOME_ROUNDED,
-                selected_icon=Icon(Icons.HOME_ROUNDED),
             ),
             Divider(thickness=2),
             NavigationDrawerDestination(
-                icon=Icon(Icons.SEARCH_ROUNDED),
+                icon=Icons.SEARCH_ROUNDED,
                 label="Search",
-                selected_icon=Icons.SEARCH_ROUNDED,
             ),
         ],
     )
 
     c_val = TextField(label="Enter company name", hint_text="Please enter text here", on_submit=result_handler)
+    lv = ListView(expand=1, spacing=10, padding=20, auto_scroll=False)
+
+    header = Row(
+        alignment=MainAxisAlignment.SPACE_BETWEEN,
+        controls=[
+            IconButton(Icons.MENU, on_click=lambda e: page.open(drawer)),
+            Row(
+                controls=[
+                    IconButton(Icons.LIGHT_MODE_OUTLINED, on_click=toggle_theme),
+                    IconButton(Icons.CONTACT_SUPPORT, on_click=lambda e: page.launch_url(
+                        "https://ishitasampat.github.io/QX25-PS2-T43-Website/"))
+                ]
+            )
+        ]
+    )
 
     home_screen = Column(
-        [
-            Container(
-                content=Column(
-                    controls=[
-                        Container(height=40),
-                        Row(
-                            alignment=MainAxisAlignment.SPACE_BETWEEN,
-                            controls=[
-                                Container(
-                                    content=IconButton(Icons.MENU, on_click=lambda e: page.open(drawer))
-                                ),
-                                Row(
-                                    controls=[
-                                        IconButton(Icons.LIGHT_MODE_OUTLINED, on_click=toggle_theme),
-                                        IconButton(Icons.CONTACT_SUPPORT, on_click=lambda e: page.launch_url("https://ishitasampat.github.io/QX25-PS2-T43-Website/"))
-                                    ]
-                                )
-                            ]
-                        ),
-
-                        Row(
-                            alignment=MainAxisAlignment.CENTER,
-                            controls=[
-                                Image(src="app_icon.png",width=150,height=150,fit =ImageFit.CONTAIN)
-                            ]
-                        ),
-                        Row(
-                            alignment=MainAxisAlignment.CENTER,
-                            controls=[
-                                Text("SLM FindMyFunds", size=24, text_align=TextAlign.CENTER, )
-                            ]
-                        ),
-                        Container(height=40),
-
-
-                        Column(
-                            alignment=MainAxisAlignment.CENTER,
-                            controls=[
-                                TextField(label="Enter Ngrok IP", hint_text="Please enter IP here", on_change=update_url,border_color="f9a56d",color="#ffffff")
-                            ]
-                        ),
-                        Row(
-                            alignment=MainAxisAlignment.CENTER,
-                            controls=[
-                                ElevatedButton("Start your Search", icon=Icons.SEARCH_OUTLINED, on_click=lambda e: switch_to_search(),bgcolor="#DC3535",color="#ffffff",icon_color="#ffffff")
-
-                            ]
-                        )
-                    ]
-                )
+        controls=[
+            header,
+            Row(
+                alignment=MainAxisAlignment.CENTER,
+                controls=[
+                    Image(src="app_icon.png", width=150, height=150, fit=ImageFit.CONTAIN),
+                ]
+            ),
+            Row(
+                alignment=MainAxisAlignment.CENTER,
+                controls=[
+                    Text("SLM FindMyFunds", size=24, text_align=TextAlign.CENTER),
+                ]
+            ),
+            Row(
+                alignment=MainAxisAlignment.CENTER,
+                controls=[
+                TextField(label="Enter Ngrok IP", hint_text="Please enter IP here", on_change=update_url),
+                ElevatedButton("Start your Search", icon=Icons.SEARCH_OUTLINED, on_click=lambda e: switch_to_search(),
+                           bgcolor="#DC3535", color="#ffffff", icon_color="#ffffff"),
+                ]
             )
         ]
     )
 
     search_screen = Column(
-        [
-            Container(
-                content=Column(
-                    controls=[
-                        Container(height=40),
-                        Row(
-                            alignment=MainAxisAlignment.SPACE_BETWEEN,
-                            controls=[
-                                Container(
-                                    content=IconButton(Icons.MENU, on_click=lambda e: page.open(drawer))
-                                ),
-                                Row(
-                                    controls=[
-                                        IconButton(Icons.LIGHT_MODE_OUTLINED, on_click=toggle_theme),
-                                        IconButton(Icons.CONTACT_SUPPORT, on_click=lambda e: page.launch_url("https://ishitasampat.github.io/QX25-PS2-T43-Website/"))
-                                    ]
-                                )
-                            ]
-                        ),
-                        Row(
-                            alignment=MainAxisAlignment.CENTER,
-                            controls=[
-                                c_val,
-                                ElevatedButton(text="Submit", on_click=result_handler),
-                            ]
-                        ),
-                        Row(
-                            alignment=MainAxisAlignment.CENTER,
-                            controls=[
-                                bar_cont,
-                            ]
-                        )
-                    ]
-                )
-            )
+        controls=[
+            header,
+            Row(
+                alignment=MainAxisAlignment.CENTER,
+                controls=[
+                    c_val,
+                    ElevatedButton(text="Submit", on_click=result_handler),
+                ]
+            ),
+            bar_cont,
+            lv,
         ]
     )
 
-    page.add(home_screen)
+    def switch_to_search():
+        page.controls.clear()
+        page.add(search_screen)
+        page.update()
 
+    page.add(home_screen)
 
 app(main)
